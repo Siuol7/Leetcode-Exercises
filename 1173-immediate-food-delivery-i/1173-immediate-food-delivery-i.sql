2c7c17b2971c77ccaf64a2dd1a3dfcbaad659713
CaoTan <101058559+Siuol7@users.noreply.github.com>
2022-12-29 04:57:22 +0200

    Time: 737 ms (29.17%), Space: 0B (100.00%) - LeetHub

--- 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -1,6 +1,3 @@
 # Write your MySQL query statement below
-select ifnull(round(((select count(delivery_id) as cnt from delivery 
-                    where order_date=customer_pref_delivery_date)
-                    /
-                    count(delivery_id))*100,2),0.00) as immediate_percentage 
-from delivery
+
+select round(100 * sum(order_date = customer_pref_delivery_date) / count(*), 2) as immediate_percentage from Delivery;
