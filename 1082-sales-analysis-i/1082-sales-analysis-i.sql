123f09949ac46833dd3f331bed8410787e5db655
CaoTan <101058559+Siuol7@users.noreply.github.com>
2022-12-29 02:48:32 +0200

    Delete 1082-sales-analysis-i directory

--- 1082-sales-analysis-i/1082-sales-analysis-i.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-with cte as(select seller_id,sum(price) as total from sales 
-group by seller_id
-order by total desc
-limit 1)
-
-select seller_id from sales
-group by seller_id
-having sum(price)=(select total from cte)
