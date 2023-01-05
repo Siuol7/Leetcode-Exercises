0917fd89627f3118e9804013b562082fbd539b29
CaoTan <101058559+Siuol7@users.noreply.github.com>
2023-01-05 06:10:46 +0200

    Time: 553 ms (73.53%), Space: 0B (100.00%) - LeetHub

--- 2339-all-the-matches-of-the-league/2339-all-the-matches-of-the-league.sql
+++ 2339-all-the-matches-of-the-league/2339-all-the-matches-of-the-league.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
 select a.team_name as home_team,b.team_name as away_team from teams a
-join teams b
+inner join teams b
 on a.team_name!=b.team_name
-order by home_team desc
