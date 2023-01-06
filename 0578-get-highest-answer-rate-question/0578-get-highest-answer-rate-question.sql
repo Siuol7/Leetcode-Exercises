b7c4602828cd4a3570b836e8cb639e4eeb23004d
CaoTan <101058559+Siuol7@users.noreply.github.com>
2023-01-06 03:47:39 +0200

    Time: 917 ms (5.07%), Space: 0B (100.00%) - LeetHub

--- 0578-get-highest-answer-rate-question/0578-get-highest-answer-rate-question.sql
+++ 0578-get-highest-answer-rate-question/0578-get-highest-answer-rate-question.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-select question_id as survey_log from surveylog
-group by question_id
-order by count(case when action='answer' then 1 end)/ count(case when action='show' then 1 end)  desc, 1 asc
-limit 1
+
+SELECT  question_id as survey_log
+FROM SurveyLog
+GROUP BY 1
+ORDER BY (count(answer_id) / count(case when action = 'show' then question_id ELSE null end )) desc, 1 asc
+LIMIT 1
