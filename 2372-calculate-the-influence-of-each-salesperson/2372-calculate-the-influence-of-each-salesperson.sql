9fd5b397a5e37f268137f2f27daa5d87a258e0bf
CaoTan <101058559+Siuol7@users.noreply.github.com>
2023-01-10 03:25:49 +0200

    Time: 1091 ms (63.37%), Space: 0B (100.00%) - LeetHub

--- 2372-calculate-the-influence-of-each-salesperson/2372-calculate-the-influence-of-each-salesperson.sql
+++ 2372-calculate-the-influence-of-each-salesperson/2372-calculate-the-influence-of-each-salesperson.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
-with sub as  (select c.salesperson_id,sum(s.price) as total from customer c
-left  join sales s
-using(customer_id)
-group by c.salesperson_id
-order by 1)
-select sp.salesperson_id,sp.name,ifnull(sb.total,0) as total from salesperson sp
-left join sub sb
-using(salesperson_id)
-order by 1
+SELECT
+    P.salesperson_id,
+    P.name,
+    IFNULL(SUM(price),0) AS total
+FROM Salesperson P
+LEFT JOIN Customer C
+    ON P.salesperson_id = C.salesperson_id
+LEFT JOIN Sales S
+    ON C.customer_id = S.customer_id
+GROUP BY 1
